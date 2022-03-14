62993f68888f825e16dd8ec40f8631a7cd0ebe08
Andy Mockler <asmockler@gmail.com>
2022-03-14 13:15:56 -0400

    Fix some annoying pointer bugs

--- client/Home/CheckButton.tsx
+++ client/Home/CheckButton.tsx
@@ -1,4 +1,5 @@
 import { CheckIcon } from "@heroicons/react/solid";
+import { MouseEvent } from "react";
 
 interface Props {
   checked: boolean;
@@ -6,10 +7,15 @@ interface Props {
 }
 
 export function CheckButton({ checked, onClick }: Props) {
+  function handleClick(event: MouseEvent<HTMLButtonElement>) {
+    event.stopPropagation();
+    onClick();
+  }
+
   return (
     <button
       className="relative h-4 w-4 flex-shrink-0 rounded-full border hover:bg-white hover:bg-opacity-20"
-      onClick={onClick}
+      onClick={handleClick}
     >
       {checked ? (
         <CheckIcon className="absolute top-1/2 left-1/2 h-3 w-3 -translate-x-1/2 -translate-y-1/2 text-white opacity-80" />
--- client/Home/NowIndicator.tsx
+++ client/Home/NowIndicator.tsx
@@ -14,7 +14,7 @@ export function NowIndicator() {
   return (
     <div
       className="
-        absolute z-10 h-[2px] w-full
+        pointer-events-none absolute z-10 h-[2px] w-full
         bg-gradient-to-r from-blue-800 via-purple-600 to-pink-500 dark:from-sky-500 dark:via-purple-400 dark:to-pink-600
       "
       style={{ top: now.getHours() * 60 + now.getMinutes() }}
